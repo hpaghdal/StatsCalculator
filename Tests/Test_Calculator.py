2bff4934079330961ecc74268fbaea26ae6c66db
utsav99599 <56970526+utsav99599@users.noreply.github.com>
2020-11-04 21:34:11 -0500

    Features: Added/Tested Subtraction function

--- Tests/Test_Calculator.py
+++ Tests/Test_Calculator.py
@@ -14,3 +14,8 @@ class MyTestCase(unittest.TestCase):
         test_data = CsvReader('Tests/Data/Addition.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+
+    def test_subtract_method_calculator(self):
+        test_data = CsvReader('Tests/Data/Subtraction.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
