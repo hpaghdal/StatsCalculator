5815383e4570911ddba891567f3e75140792b33e
utsav99599 <56970526+utsav99599@users.noreply.github.com>
2020-11-08 11:14:24 -0500

    Features: Added/Tested Random Number Without Seed

---
+++ Random Generator function/RandomNumberNoSeed.py
@@ -0,0 +1,20 @@
+import random
+import numpy as np
+
+
+def random_integer(num):
+    nlist = (range(0, 9))
+    number_list = random.sample(nlist, num)
+    return "Random integer is", number_list
+
+
+print(random_integer(1))
+
+
+def random_float(num):
+    nlist = (np.arange(0.0, 9.0))
+    ran_float = random.uniform(nlist, num)
+    return "Random float is", ran_float
+
+
+print(random_float(1.0))
